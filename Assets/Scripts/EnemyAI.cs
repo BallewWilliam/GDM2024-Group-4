f8ebe8e084cd237723dd10d0a95a675354ba1cff
Kaden D. White <kadendw23@gmail.com>
2024-03-06 18:03:22 -0600

    Enemy AI Random

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,22 +5,58 @@ using UnityEngine.UI;
 
 public class EnemyAI : MonoBehaviour
 {
-	public Button button1;
-	public Button button2;
-	public Button button3;
-	public Button button4;
-	public Button button5;
-	public Button button6;
-	public Button button7;
-	public Button button8;
-	public Button button9;
-	public Button button10;
-	public Button button11;
-	public Button button12;
+	public float RadNum = 0f;
+	public bool Steel2;
+	public bool Tornado2;
+	public bool Steam2;
+	public bool Flamethrower2;
+	public bool Ice2;
+	public bool Storm2;
 
-	void Start()
+	public void FixedUpdate()
 	{
-		
+		RNG();
+		Spells();
+	}
+
+	public void RNG()
+	{
+		RadNum = Random.Range(1,7);
+	}
+
+	public void Spells()
+	{
+		if(RadNum == 1)
+		{
+			Steel2 = true;
+			Debug.Log("Steel2 Selected!");
+		}
+		if (RadNum == 2)
+		{
+			Tornado2 = true;
+			Debug.Log("Tornado2 Selected!");
+		}
+		if (RadNum == 3)
+		{
+			Steam2 = true;
+			Debug.Log("Steam2 Selected!");
+		}
+		if (RadNum == 4)
+		{
+			Flamethrower2 = true;
+			Debug.Log("Flamethrower2 Selected!");
+
+		}
+		if (RadNum == 5)
+		{
+			Ice2 = true;
+			Debug.Log("Ice2 Selected!");
+		}
+		if (RadNum == 6)
+		{
+			Storm2 = true;
+			Debug.Log("Storm2 Selected!");
+		}
 	}
 }
 
