31d2cc8f3e00f1f203befd3b45db79086beb79d0
Connor Ballew <conmon119@gmail.com>
2024-03-06 18:54:44 -0600

    Damage and Combination Systems Complete
    Everything is on C_Damage Scene

--- Assets/Scripts/CombinationDisplay.cs
+++ Assets/Scripts/CombinationDisplay.cs
@@ -8,138 +8,344 @@ using TMPro;
 
 public class CombinationDisplay : MonoBehaviour
 {
-    public TMP_Text element;
-    public TMP_Text element_1;
-    public TMP_Text element_2;
-    public GameObject HealthBar;
-    public bool element1;
-    public bool element2;
-    public bool air;
-    public bool water;
-    public bool fire;
-    public bool earth;
+    public TMP_Text CombinationP1;
+    public TMP_Text element_1_P1;
+    public TMP_Text element_2_P1;
+    public TMP_Text CombinationP2;
+    public TMP_Text element_1_P2;
+    public TMP_Text element_2_P2;
+    public GameObject Manager;
+    private Spell_Comparison spell;
+    public bool P1_element1;
+    public bool P1_element2;
+    public bool P2_element1;
+    public bool P2_element2;
+    public bool airP1;
+    public bool waterP1;
+    public bool fireP1;
+    public bool earthP1;
+    public bool airP2;
+    public bool waterP2;
+    public bool fireP2;
+    public bool earthP2;
 
     // Start is called before the first frame update
     void Start()
     {
-        element1 = false; element2 = false;
-        element = GetComponent<TextMeshProUGUI>();
+        P1_element1 = false;
+        P1_element2 = false;
+        spell = Manager.GetComponent<Spell_Comparison>();
 
     }
 
     private void FixedUpdate()
     {
-        Combine();
-        if (element_1.text == element_2.text)
+        CombineP1();
+        CombineP2();
+        if (element_1_P1.text == element_2_P1.text)
         {
-            element2 = false;
+            P1_element2 = false;
+        }
+        if (element_1_P2.text == element_2_P2.text)
+        {
+            P2_element2 = false;
         }
     }
 
-    public void PrintFire()
+    public void PrintFireP1()
     {
-        fire = true;
-        if (!element1)
+        fireP1 = true;
+        if (!P1_element1)
         {
-            element_1.text = "Fire";
-            element1 = true;
-            Debug.Log("Element 1 true!");
+            element_1_P1.text = "Fire";
+            P1_element1 = true;
+            //Debug.Log("Element 1 true!");
         }
-        else if (element1 && !element2)
+        else if (P1_element1 && !P1_element2)
         {
-            element_2.text = "Fire";
-            element2 = true;
-            Debug.Log("Element 2 true!");
+            element_2_P1.text = "Fire";
+            P1_element2 = true;
+           //Debug.Log("Element 2 true!");
         }
     }
-    public void PrintWater()
+    public void PrintFireP2()
     {
-        water = true;
-        if (!element1)
+        fireP2 = true;
+        if (!P2_element1)
         {
-            element_1.text = "Water";
-            element1 = true;
+            element_1_P2.text = "Fire";
+            P2_element1 = true;
+            //Debug.Log("Element 1 true!");
         }
-        else if (element1 && !element2)
+        else if (P2_element1 && !P2_element2)
         {
-            element_2.text = "Water";
-            element2 = true;
-            Debug.Log("Element 2 true!");
+            element_2_P2.text = "Fire";
+            P2_element2 = true;
+            //Debug.Log("Element 2 true!");
         }
     }
-    public void PrintEarth()
+    public void PrintWaterP1()
     {
-        earth = true;
-        if (!element1)
+        waterP1 = true;
+        if (!P1_element1)
         {
-            element_1.text = "Earth";
-            element1 = true;
+            element_1_P1.text = "Water";
+            P1_element1 = true;
         }
-        else if (element1 && !element2)
+        else if (P1_element1 && !P1_element2)
         {
-            element_2.text = "Earth";
-            element2 = true;
-            Debug.Log("Element 2 true!");
+            element_2_P1.text = "Water";
+            P1_element2 = true;
+            //Debug.Log("Element 2 true!");
         }
     }
-    public void PrintAir()
+    public void PrintWaterP2()
     {
-        air = true;
-        if (!element1)
+        waterP2 = true;
+        if (!P2_element1)
         {
-            element_1.text = "Air";
-            element1 = true;
+            element_1_P2.text = "Water";
+            P2_element1 = true;
         }
-        else if (element1 && !element2)
+        else if (P2_element1 && !P2_element2)
         {
-            element_2.text = "Air";
-            element2 = true;
-            Debug.Log("Element 2 true!");
+            element_2_P2.text = "Water";
+            P2_element2 = true;
+            //Debug.Log("Element 2 true!");
         }
     }
-    public void Combine()
+    public void PrintEarthP1()
     {
-        if (air && water)
+        earthP1 = true;
+        if (!P1_element1)
         {
-            element.text = "Storm";
-            air = false;
-            water = false;
-            element1 = false; element2 = false;
+            element_1_P1.text = "Earth";
+            P1_element1 = true;
         }
-        if (air && earth)
+        else if (P1_element1 && !P1_element2)
         {
-            element.text = "Tornado";
-            air = false;
-            earth = false;
-            element1 = false; element2 = false;
+            element_2_P1.text = "Earth";
+            P1_element2 = true;
+            //Debug.Log("Element 2 true!");
         }
-        if (air && fire)
+    }
+    public void PrintEarthP2()
+    {
+        earthP2 = true;
+        if (!P2_element1)
         {
-            element.text = "Flamethrower";
-            air = false;
-            fire = false;
-            element1 = false; element2 = false;
+            element_1_P2.text = "Earth";
+            P2_element1 = true;
         }
-        if (fire && water)
+        else if (P2_element1 && !P2_element2)
         {
-            element.text = "Steam";
-            fire = false;
-            water = false;
-            element1 = false; element2 = false;
+            element_2_P2.text = "Earth";
+            P2_element2 = true;
+            //Debug.Log("Element 2 true!");
         }
-        if (earth && water)
+    }
+    public void PrintAirP1()
+    {
+        airP1 = true;
+        if (!P1_element1)
         {
-            element.text = "Ice";
-            earth = false;
-            water = false;
-            element1 = false; element2 = false;
+            element_1_P1.text = "Air";
+            P1_element1 = true;
         }
-        if (earth && fire)
+        else if (P1_element1 && !P1_element2)
         {
-            element.text = "Steel";
-            earth = false;
-            fire = false;
-            element1 = false; element2 = false;
+            element_2_P1.text = "Air";
+            P1_element2 = true;
+            //Debug.Log("Element 2 true!");
+        }
+    }
+    public void PrintAirP2()
+    {
+        airP2 = true;
+        if (!P2_element1)
+        {
+            element_1_P2.text = "Air";
+            P2_element1 = true;
+        }
+        else if (P2_element1 && !P2_element2)
+        {
+            element_2_P2.text = "Air";
+            P2_element2 = true;
+            //Debug.Log("Element 2 true!");
+        }
+    }
+    public void CombineP1()
+    {
+        if (airP1 && waterP1)
+        {
+            CombinationP1.text = "Storm";
+            spell.Storm1 = true;
+            spell.Tornado1 = false;
+            spell.Flamethrower1 = false;
+            spell.Steam1 = false;
+            spell.Ice1 = false;
+            spell.Steel1 = false;
+            airP1 = false;
+            waterP1 = false;
+            P1_element1 = false; 
+            P1_element2 = false;
+        }
+        if (airP1 && earthP1)
+        {
+            CombinationP1.text = "Tornado";
+            spell.Tornado1 = true;
+            spell.Storm1 = false;
+            spell.Flamethrower1 = false;
+            spell.Steam1 = false;
+            spell.Ice1 = false;
+            spell.Steel1 = false;
+            airP1 = false;
+            earthP1 = false;
+            P1_element1 = false; 
+            P1_element2 = false;
+        }
+        if (airP1 && fireP1)
+        {
+            CombinationP1.text = "Flamethrower";
+            spell.Flamethrower1 = true;
+            spell.Storm1 = false;
+            spell.Tornado1 = false;
+            spell.Steam1 = false;
+            spell.Ice1 = false;
+            spell.Steel1 = false;
+            airP1 = false;
+            fireP1 = false;
+            P1_element1 = false; 
+            P1_element2 = false;
+        }
+        if (fireP1 && waterP1)
+        {
+            CombinationP1.text = "Steam";
+            spell.Steam1 = true;
+            spell.Storm1 = false;
+            spell.Tornado1 = false;
+            spell.Flamethrower1 = false;
+            spell.Ice1 = false;
+            spell.Steel1 = false;
+            fireP1 = false;
+            waterP1 = false;
+            P1_element1 = false; 
+            P1_element2 = false;
+        }
+        if (earthP1 && waterP1)
+        {
+            CombinationP1.text = "Ice";
+            spell.Ice1 = true;
+            spell.Storm1 = false;
+            spell.Tornado1 = false;
+            spell.Flamethrower1 = false;
+            spell.Steam1 = false;
+            spell.Steel1 = false;
+            earthP1 = false;
+            waterP1 = false;
+            P1_element1 = false; 
+            P1_element2 = false;
+        }
+        if (earthP1 && fireP1)
+        {
+            CombinationP1.text = "Steel";
+            spell.Steel1 = true;
+            spell.Storm1 = false;
+            spell.Tornado1 = false;
+            spell.Flamethrower1 = false;
+            spell.Steam1 = false;
+            spell.Ice1 = false;
+            earthP1 = false;
+            fireP1 = false;
+            P1_element1 = false; 
+            P1_element2 = false;
+        }
+    }
+    public void CombineP2()
+    {
+        if (airP2 && waterP2)
+        {
+            CombinationP2.text = "Storm";
+            spell.Storm2 = true;
+            spell.Tornado2 = false;
+            spell.Flamethrower2 = false;
+            spell.Steam2 = false;
+            spell.Ice2 = false;
+            spell.Steel2 = false;
+            airP2 = false;
+            waterP2 = false;
+            P2_element1 = false; 
+            P2_element2 = false;
+        }
+        if (airP2 && earthP2)
+        {
+            CombinationP2.text = "Tornado";
+            spell.Tornado2 = true;
+            spell.Storm2 = false;
+            spell.Flamethrower2 = false;
+            spell.Steam2 = false;
+            spell.Ice2 = false;
+            spell.Steel2 = false;
+            airP2 = false;
+            earthP2 = false;
+            P2_element1 = false; 
+            P2_element2 = false;
+        }
+        if (airP2 && fireP2)
+        {
+            CombinationP2.text = "Flamethrower";
+            spell.Flamethrower2 = true;
+            spell.Storm2 = false;
+            spell.Tornado2 = false;
+            spell.Steam2 = false;
+            spell.Ice2 = false;
+            spell.Steel2 = false;
+            airP2 = false;
+            fireP2 = false;
+            P2_element1 = false; 
+            P2_element2 = false;
+        }
+        if (fireP2 && waterP2)
+        {
+            CombinationP2.text = "Steam";
+            spell.Steam2 = true;
+            spell.Storm2 = false;
+            spell.Tornado2 = false;
+            spell.Flamethrower2 = false;
+            spell.Ice2 = false;
+            spell.Steel2 = false;
+            fireP2 = false;
+            waterP2 = false;
+            P2_element1 = false; 
+            P2_element2 = false;
+        }
+        if (earthP2 && waterP2)
+        {
+            CombinationP2.text = "Ice";
+            spell.Ice2 = true;
+            spell.Storm2 = false;
+            spell.Tornado2 = false;
+            spell.Flamethrower2 = false;
+            spell.Steam2 = false;
+            spell.Steel2 = false;
+            earthP2 = false;
+            waterP2 = false;
+            P2_element1 = false; 
+            P2_element2 = false;
+        }
+        if (earthP2 && fireP2)
+        {
+            CombinationP2.text = "Steel";
+            spell.Steel2 = true;
+            spell.Storm2 = false;
+            spell.Tornado2 = false;
+            spell.Flamethrower2 = false;
+            spell.Steam2 = false;
+            spell.Ice2 = false;
+            earthP2 = false;
+            fireP2 = false;
+            P2_element1 = false; 
+            P2_element2 = false;
         }
     }
 }
---
+++ Assets/Scripts/Spell_Comparison.cs
@@ -0,0 +1,367 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spell_Comparison : MonoBehaviour
+{
+    public bool Steel1;
+    public bool Steel2;
+    public bool Tornado1;
+    public bool Tornado2;
+    public bool Steam1;
+    public bool Steam2;
+    public bool Flamethrower1;
+    public bool Flamethrower2;
+    public bool Ice1;
+    public bool Ice2;
+    public bool Storm1;
+    public bool Storm2;
+    public Image P1_Health;
+    public Image P2_Health;
+    public float P1_Health_Amount;
+    public float P2_Health_Amount;
+
+    private void FixedUpdate()
+    {
+        Compare();
+    }   
+
+    void Compare()
+    {
+        if (Steel1 &&  Steel2)
+        {
+            Steel1 = false;
+            Steel2 = false;
+        }
+        if (Steel1 && Tornado2)
+        {
+            P2_Health_Amount -= 2f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            Steel1 = false;
+            Tornado2 = false;
+        }
+        if (Steel1 && Steam2)
+        {
+            P2_Health_Amount -= 1.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 0.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steel1 = false;
+            Steam2 = false;
+        }
+        if (Steel1 && Flamethrower2)
+        {
+            P2_Health_Amount -= 1f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steel1 = false;
+            Flamethrower2 = false;
+        }
+        if (Steel1 && Ice2)
+        {
+            P2_Health_Amount -= 0.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steel1 = false;
+            Ice2 = false;
+        }
+        if (Steel1 && Storm2)
+        {
+            P1_Health_Amount -= 2f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steel1 = false;
+            Storm2 = false;
+        }
+
+        if (Tornado1 && Steel2)
+        {
+            P1_Health_Amount -= 2f;
+            P1_Health.fillAmount = P2_Health_Amount / 10;
+            Tornado1 = false;
+            Steel2 = false;
+        }
+        if (Tornado1 && Tornado2)
+        {
+            Tornado1 = false;
+            Tornado2 = false;
+        }
+        if (Tornado1 && Steam2)
+        {
+            P2_Health_Amount -= 2f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            Tornado1 = false;
+            Steam2 = false;
+        }
+        if (Tornado1 && Flamethrower2)
+        {
+            P2_Health_Amount -= 1.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 0.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Tornado1 = false;
+            Flamethrower2 = false;
+        }
+        if (Tornado1 && Ice2)
+        {
+            P2_Health_Amount -= 1f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Tornado1 = false;
+            Ice2 = false;
+        }
+        if (Tornado1 && Storm2)
+        {
+            P2_Health_Amount -= 0.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Tornado1 = false;
+            Storm2 = false;
+        }
+
+        if (Steam1 && Steel2)
+        {
+            P2_Health_Amount -= 0.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steam1 = false;
+            Steel2 = false;
+        }
+        if (Steam1 && Tornado2)
+        {
+            P1_Health_Amount -= 2f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steam1 = false;
+            Tornado2 = false;
+        }
+        if (Steam1 && Steam2)
+        {
+            Steam1 = false;
+            Steam2 = false;
+        }
+        if (Steam1 && Flamethrower2)
+        {
+            P2_Health_Amount -= 2f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            Steam1 = false;
+            Flamethrower2 = false;
+        }
+        if (Steam1 && Ice2)
+        {
+            P2_Health_Amount -= 1.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 0.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steam1 = false;
+            Ice2 = false;
+        }
+        if (Steam1 && Storm2)
+        {
+            P2_Health_Amount -= 1f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Steam1 = false;
+            Storm2 = false;
+        }
+
+        if (Flamethrower1 && Steel2)
+        {
+            P2_Health_Amount -= 1f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Flamethrower1 = false;
+            Steel2 = false;
+        }
+        if (Flamethrower1 && Tornado2)
+        {
+            P2_Health_Amount -= 0.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Flamethrower1 = false;
+            Tornado2 = false;
+        }
+        if (Flamethrower1 && Steam2)
+        {
+            P1_Health_Amount -= 2f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Flamethrower1 = false;
+            Steam2 = false;
+        }
+        if (Flamethrower1 && Flamethrower2)
+        {
+            Flamethrower1 = false;
+            Flamethrower2 = false;
+        }
+        if (Flamethrower1 && Ice2)
+        {
+            P2_Health_Amount -= 2f;
+            P2_Health.fillAmount = P1_Health_Amount / 10;
+            Flamethrower1 = false;
+            Ice2 = false;
+        }
+        if (Flamethrower1 && Storm2)
+        {
+            P2_Health_Amount -= 1.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 0.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Flamethrower1 = false;
+            Storm2 = false;
+        }
+
+        if (Ice1 && Steel2)
+        {
+            P2_Health_Amount -= 1.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 0.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Ice1 = false;
+            Steel2 = false;
+        }
+        if (Ice1 && Tornado2)
+        {
+            P2_Health_Amount -= 1f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Ice1 = false;
+            Tornado2 = false;
+        }
+        if (Ice1 && Steam2)
+        {
+            P2_Health_Amount -= 0.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Ice1 = false;
+            Steam2 = false;
+        }
+        if (Ice1 && Flamethrower2)
+        {
+            P1_Health_Amount -= 2f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Ice1 = false;
+            Flamethrower2 = false;
+        }
+        if (Ice1 && Ice2)
+        {
+            Ice1 = false;
+            Ice2 = false;
+        }
+        if (Ice1 && Storm2)
+        {
+            P2_Health_Amount -= 2f;
+            P2_Health.fillAmount = P1_Health_Amount / 10;
+            Ice1 = false;
+            Storm2 = false;
+        }
+
+        if (Storm1 && Steel2)
+        {
+            P2_Health_Amount -= 2f;
+            P2_Health.fillAmount = P1_Health_Amount / 10;
+            Storm1 = false;
+            Steel2 = false;
+        }
+        if (Storm1 && Tornado2)
+        {
+            P2_Health_Amount -= 1.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 0.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Storm1 = false;
+            Tornado2 = false;
+        }
+        if (Storm1 && Steam2)
+        {
+            P2_Health_Amount -= 1f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Storm1 = false;
+            Steam2 = false;
+        }
+        if (Storm1 && Flamethrower2)
+        {
+            P2_Health_Amount -= 0.5f;
+            P2_Health.fillAmount = P2_Health_Amount / 10;
+            P1_Health_Amount -= 1.5f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Storm1 = false;
+            Flamethrower2 = false;
+        }
+        if (Storm1 && Ice2)
+        {
+            P1_Health_Amount -= 2f;
+            P1_Health.fillAmount = P1_Health_Amount / 10;
+            Storm1 = false;
+            Ice2 = false;
+        }
+        if (Storm1 && Storm2)
+        {
+            Storm1 = false;
+            Storm2 = false;
+        }
+
+    }
+
+    public void Steel1T()
+    { 
+        Steel1 = true;
+
+    }
+    public void Steel2T() 
+    {
+        Steel2 = true;
+    }
+    public void Tornado1T()
+    {
+        Tornado1 = true;
+    }
+    public void Tornado2T()
+    {
+        Tornado2 = true;
+    }
+    public void Steam1T()
+    {
+        Steam1 = true;
+    }
+    public void Steam2T()
+    {
+        Steam2 = true;
+    }
+    public void Flamethrower1T()
+    {
+        Flamethrower1 = true;
+    }
+    public void Flamethrower2T()
+    {
+        Flamethrower2 = true;
+    }
+    public void Ice1T()
+    {
+        Ice1 = true;
+    }
+    public void Ice2T()
+    {
+        Ice2 = true;
+    }
+    public void Storm1T()
+    {
+        Storm1 = true;
+    }
+    public void Storm2T()
+    {
+        Storm2 = true;
+    }
+
+
+}
