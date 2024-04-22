4bd903a3b55b5492d316e544f5835b070141efdf
Connor Ballew <conmon119@gmail.com>
2024-04-22 17:27:49 -0500

    Animation script changes

--- Assets/Scripts/AnimationControlRightPlayer.cs
+++ Assets/Scripts/AnimationControlRightPlayer.cs
@@ -2,51 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationControl : MonoBehaviour
+public class AnimationControlRightPlayer : MonoBehaviour
 {
-	public GameObject TornadoPrefab; // Prefab of the object to instantiate
-	public GameObject SteelPrefab;                               //public Transform elementPosition; // Position to instantiate the object
-	public GameObject SteamPrefab;                               //public Transform movementPosition;
-	public GameObject IcePrefab;
-	public GameObject StormPrefab;
-	public GameObject FlamethrowerPrefab;
+    public GameObject TornadoPrefab; // Prefab of the object to instantiate
+    public GameObject SteelPrefab;                               //public Transform elementPosition; // Position to instantiate the object
+    public GameObject SteamPrefab;                               //public Transform movementPosition;
+    public GameObject IcePrefab;
+    public GameObject StormPrefab;
+    public GameObject FlamethrowerPrefab;
 
-	public GameObject Manager;
-	private Spell_Comparison spell;
+    public GameObject Manager;
+    private Spell_Comparison spell;
 
-	public float elementSpeed = 0f;
+    public float elementSpeed = 0f;
 
-	public float destroyDelay; // Delay before destroying the instantiated object
+    public float destroyDelay; // Delay before destroying the instantiated object
 
-	private bool hasElementBeenInstantiated = false; // Flag to track if an element has been instantiated
+    private bool hasElementBeenInstantiated = false; // Flag to track if an element has been instantiated
     private void Start()
     {
         spell = Manager.GetComponent<Spell_Comparison>();
     }
     void FixedUpdate()
-	{
-		// Check if the input button is pressed and an element hasn't been instantiated yet
-		if (spell.Tornado1 && !hasElementBeenInstantiated)
-		{
-			// Instantiate the object
-			GameObject newElement = Instantiate(TornadoPrefab, transform.position, transform.rotation);
-			Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
-			elementTransform.velocity = transform.right * elementSpeed;
+    {
+        // Check if the input button is pressed and an element hasn't been instantiated yet
+        if (spell.Tornado2 && !hasElementBeenInstantiated)
+        {
+            // Instantiate the object
+            GameObject newElement = Instantiate(TornadoPrefab, transform.position, transform.rotation);
+            Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
+            elementTransform.velocity = -transform.right * elementSpeed;
 
-			newElement.name = "Tornado";
+            newElement.name = "Tornado";
 
-			// Set the flag to true
-			hasElementBeenInstantiated = true;
+            // Set the flag to true
+            hasElementBeenInstantiated = true;
 
-			// Start a coroutine to destroy the instantiated object after a delay
-			StartCoroutine(DestroyElement(newElement));
-		}
-        if (spell.Steel1 && !hasElementBeenInstantiated)
+            // Start a coroutine to destroy the instantiated object after a delay
+            StartCoroutine(DestroyElement(newElement));
+        }
+        if (spell.Steel2 && !hasElementBeenInstantiated)
         {
             // Instantiate the object
             GameObject newElement = Instantiate(SteelPrefab, transform.position, transform.rotation);
             Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
-            elementTransform.velocity = transform.right * elementSpeed;
+            elementTransform.velocity = -transform.right * elementSpeed;
 
             newElement.name = "Steel";
 
@@ -56,12 +56,12 @@ public class AnimationControl : MonoBehaviour
             // Start a coroutine to destroy the instantiated object after a delay
             StartCoroutine(DestroyElement(newElement));
         }
-        if (spell.Steam1 && !hasElementBeenInstantiated)
+        if (spell.Steam2 && !hasElementBeenInstantiated)
         {
             // Instantiate the object
             GameObject newElement = Instantiate(SteamPrefab, transform.position, transform.rotation);
             Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
-            elementTransform.velocity = transform.right * elementSpeed;
+            elementTransform.velocity = -transform.right * elementSpeed;
 
             newElement.name = "Steam";
 
@@ -71,12 +71,12 @@ public class AnimationControl : MonoBehaviour
             // Start a coroutine to destroy the instantiated object after a delay
             StartCoroutine(DestroyElement(newElement));
         }
-        if (spell.Ice1 && !hasElementBeenInstantiated)
+        if (spell.Ice2 && !hasElementBeenInstantiated)
         {
             // Instantiate the object
             GameObject newElement = Instantiate(IcePrefab, transform.position, transform.rotation);
             Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
-            elementTransform.velocity = transform.right * elementSpeed;
+            elementTransform.velocity = -transform.right * elementSpeed;
 
             newElement.name = "Ice";
 
@@ -86,12 +86,12 @@ public class AnimationControl : MonoBehaviour
             // Start a coroutine to destroy the instantiated object after a delay
             StartCoroutine(DestroyElement(newElement));
         }
-        if (spell.Storm1 && !hasElementBeenInstantiated)
+        if (spell.Storm2 && !hasElementBeenInstantiated)
         {
             // Instantiate the object
             GameObject newElement = Instantiate(StormPrefab, transform.position, transform.rotation);
             Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
-            elementTransform.velocity = transform.right * elementSpeed;
+            elementTransform.velocity = -transform.right * elementSpeed;
 
             newElement.name = "Storm";
 
@@ -101,12 +101,12 @@ public class AnimationControl : MonoBehaviour
             // Start a coroutine to destroy the instantiated object after a delay
             StartCoroutine(DestroyElement(newElement));
         }
-        if (spell.Flamethrower1 && !hasElementBeenInstantiated)
+        if (spell.Flamethrower2 && !hasElementBeenInstantiated)
         {
             // Instantiate the object
             GameObject newElement = Instantiate(FlamethrowerPrefab, transform.position, transform.rotation);
             Rigidbody2D elementTransform = newElement.GetComponent<Rigidbody2D>();
-            elementTransform.velocity = transform.right * elementSpeed;
+            elementTransform.velocity = -transform.right * elementSpeed;
 
             newElement.name = "Flamethrower";
 
@@ -118,15 +118,15 @@ public class AnimationControl : MonoBehaviour
         }
     }
 
-	IEnumerator DestroyElement(GameObject element)
-	{
-		// Wait for the specified delay
-		yield return new WaitForSeconds(destroyDelay);
+    IEnumerator DestroyElement(GameObject element)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(destroyDelay);
 
-		// Destroy the instantiated object
-		Destroy(element);
+        // Destroy the instantiated object
+        Destroy(element);
 
-		// Reset the flag
-		hasElementBeenInstantiated = false;
-	}
+        // Reset the flag
+        hasElementBeenInstantiated = false;
+    }
 }
