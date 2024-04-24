3199bdb3967db09b9a51cc4a947ead2b06b223ad
Kaden D. White <kadendw23@gmail.com>
2024-04-24 17:21:18 -0500

    Singleplayer Scene
    Art and mechanics combined

--- Assets/Scripts/QuitScript.cs
+++ Assets/Scripts/QuitScript.cs
@@ -11,7 +11,7 @@ public class QuitScript : MonoBehaviour
     }
     public void RestartButton()
     {
-        SceneManager.LoadScene("C_Damage");
+        SceneManager.LoadScene("C_Damage Art Scene");
     }
     public void multiplayerRestart()
     {
